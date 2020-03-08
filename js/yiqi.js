9f41dacbe0adca64e5dcc18eb3285fb3a8b68dc1
huahua123555 <61927637+huahua123555@users.noreply.github.com>
2020-03-08 20:35:10 +0800

    Add files via upload

---
+++ js/yiqi.js
@@ -0,0 +1,34 @@
+﻿// JavaScript Document
+var Dianji=0;
+window.onload=function(){
+	var buhao = document.getElementById("buhao");
+	var hao = document.getElementById("hao");
+	buhao.onclick=function(){
+		Dianji++;
+		if(Dianji==1){
+	   			alert("在考虑一下呗");
+	   }else if(Dianji==2){
+		   		alert("天天准时上线学习");
+		   		
+	   }else if(Dianji==3){
+		   		alert("每天不熬夜");
+		   		
+	   }else if(Dianji==4){
+		   		alert("家务我全干");
+		   		
+	  }else if(Dianji==5){
+		   		
+	  }else if(Dianji==6){
+		   		
+	  }else if(Dianji==7){
+		   		alert("过年钱全上交");
+		  		Dianji=1;
+	  }
+
+	}
+	hao.onclick=function(){
+		alert("给我钱呗");
+	}
+	
+
+}
